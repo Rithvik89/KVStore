2594059c4014d5cc780fe786786503c19763365d
Rithvik89 <rithvikalkanti@gmail.com>
2025-04-14 23:59:20 +0530

    Enhance WAL write functionality with conflict detection and error handling; add Zookeeper integration for version retrieval

--- wal.go
+++ wal.go
@@ -15,34 +15,43 @@ type WAL struct {
 	SuccessMarker bool   `json:"success_marker"`
 }
 
-func (app *App) writeToWAL(wal WAL) {
+func (app *App) writeToWAL(wal WAL) (int32, error) {
 	// Open the WAL file for appending
 	file, err := os.OpenFile(fmt.Sprintf("wal_%d.log", app.KvPort), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Println("Failed to open WAL file:", err)
-		return
+		return -1, err
 	}
 	defer file.Close()
 
-	// Increment the write version
-	app.WriteVersionMutex.Lock()
-	wal.Version = app.WriteVersion
-	app.WriteVersion++
-	app.WriteVersionMutex.Unlock()
+	// Check for conflicts
+	// Get the last successful write version from Zookeeper
 
-	// Set the success marker
-	wal.SuccessMarker = false
+	latestVersion := app.readLatestSuccessfulWriteVersionFromWAL()
+	latestSuccessfulVersion, err := app.readLastestSuccessfulWriteVersionFromZK()
+	if err != nil {
+		log.Println("Failed to get latest successful write version from Zookeeper:", err)
+		return -1, err
+	}
+
+	if latestSuccessfulVersion != latestVersion {
+		// Conflict detected
+		log.Println("Conflict detected: latest successful version from Zookeeper does not match WAL version")
+		//TODO: Conflict resolution logic can be added here
+		return -1, fmt.Errorf("conflict detected: latest successful version from Zookeeper does not match WAL version")
+
+	}
 
 	// Write the WAL entry to the file
 	err = json.NewEncoder(file).Encode(wal)
 	if err != nil {
 		log.Println("Failed to write to WAL file:", err)
-		return
+		return -1, err
 	}
-
+	return wal.Version, nil
 }
 
-func (app *App) readLatestSuccessfulWriteVersion() int32 {
+func (app *App) readLatestSuccessfulWriteVersionFromWAL() int32 {
 	// Open the WAL file for reading
 	file, err := os.Open(fmt.Sprintf("wal_%d.log", app.KvPort))
 	if err != nil {
@@ -51,7 +60,7 @@ func (app *App) readLatestSuccessfulWriteVersion() int32 {
 	}
 	defer file.Close()
 
-	var latestVersion int32 = 1
+	var latestVersion int32
 	var wal WAL
 
 	// Read the WAL entries from the file
@@ -60,13 +69,25 @@ func (app *App) readLatestSuccessfulWriteVersion() int32 {
 		if err != nil {
 			break
 		}
-
 		//TODO: This should only happen if there is a success marker
 		latestVersion = wal.Version
-
 	}
-
-	log.Println("Latest successful write version:", latestVersion)
-
 	return latestVersion
+}
+
+func (app *App) readLastestSuccessfulWriteVersionFromZK() (int32, error) {
+	path := "/version"
+	var latestVersion int32
+	// Get the latest successful write version from Zookeeper
+	data, _, err := app.ZkClient.Get(path)
+	if err != nil {
+		log.Println("Failed to get latest successful write version from Zookeeper:", err)
+		return -1, err
+	}
+	err = json.Unmarshal(data, &latestVersion)
+	if err != nil {
+		log.Println("Failed to unmarshal latest successful write version:", err)
+		return -1, err
+	}
+	return latestVersion, nil
 }
